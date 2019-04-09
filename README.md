00a959750dc81267482251774ae63c05829cd7b9
Anderson Pimentel <andbrain@gmail.com>
2019-04-09 16:47:07 -0400

    feat: load file tests

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # jflap-grades
 Grades for FTC. The usage of JFLPLib CLI to compare results and output grades
+
+## Tests
+
+1. Name and number of test files must be the same for referenced in students questions sufix.
+e.g:
+- tests
+-- q01a.tst 
+- students
+-- student1
+--- q01a.jff
+
+2. Test files must have the input tests and the expected result, as bellow:
+- tests
+-- q01a.tst
+input	result
+001	0
+110	1
+
+where:
+1 -> accept
+0 -> reject
--- lista01/grades01.py
+++ lista01/grades01.py
@@ -3,16 +3,33 @@ from os import listdir
 from os.path import isfile, join, splitext
 
 jflapJarPath = '../jflaplib-cli-1.3-bundle.jar'
+testsFilePath = 'tests'
 
 def runJFlapLibCLI(method, file, input):
     result = subprocess.run(['java', '-jar', jflapJarPath, method, file, input], stdout=subprocess.PIPE)
     return result.stdout.decode('ascii')
 
 def readQuestions():
-    files = [f for f in listdir('tests') if isfile(join('tests', f))]
+    files = [f for f in listdir(testsFilePath) if isfile(join(testsFilePath, f))]
+    files.sort()
     filesWext = [splitext(f)[0] for f in files]
-    filesWext.sort()
-    return filesWext
+    return filesWext, files
+
+def loadTestFiles(testFiles):
+    tests = []
+    for test in testFiles:
+        questionTest = []
+        with open(join(testsFilePath,test)) as fp:
+            for line in fp:
+                line = line.rstrip('\n').split(' ')
+                if(len(line) != 2):
+                    print('[ERROR] Test should have input and expect result, eg.: 001 1')
+                    print('[EFILE]', join(testsFilePath,test))
+                    exit(1)
+                questionTest.append(line)  # rstrip -> it removes new line '\n' from string
+            fp.close()
+        tests.append(questionTest)
+    return tests
 
 def main():
     file = 'alunos/anderson-pimentel/Anderson_L01_q01a.jff'
@@ -20,7 +37,14 @@ def main():
     method = 'run'
     print(runJFlapLibCLI(method,file,input))
 
-    questions = readQuestions()
+    # read questions and test files
+    questions, testFiles = readQuestions()
     print(questions)
+    print(testFiles)
+
+    # Load test files
+    tests = loadTestFiles(testFiles)
+    print(tests)
+
 if __name__== "__main__":
     main()
