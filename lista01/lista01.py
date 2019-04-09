e385191172118dd7154e8189526f9dda840b36bb
Anderson Pimentel <andbrain@gmail.com>
2019-04-09 14:40:22 -0400

    cleaning output of jflapLib CLI

--- lista01/lista01.py
+++ lista01/lista01.py
@@ -2,12 +2,11 @@ import subprocess
 
 jflapJarPath = '../jflaplib-cli-1.3-bundle.jar'
 
-def main():
-  print("Hello World!")
-  # result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
-  result = subprocess.run(['java', '-jar', jflapJarPath, 'run', 'alunos/anderson-pimentel/Anderson_L01_q01a.jff', '010101'], stdout=subprocess.PIPE)
 
-  print(result.stdout)
+
+def main():
+  result = subprocess.run(['java', '-jar', jflapJarPath, 'run', 'alunos/anderson-pimentel/Anderson_L01_q01a.jff', '010101'], stdout=subprocess.PIPE)
+  print(result.stdout.decode('ascii'))
 
 if __name__== "__main__":
   main()
